404e8be685b82569b1e75de91ec2c2daa8fce0d0
officialprakashkumarsingh <officialprakashkrsingh@gmail.com>
2025-07-30 00:07:22 +0530

    feat: previous task: delete all the files modified files: canvas.h, ctx.c, index.h, script.js, style.c
    yes, file is still with code complete clean it

--- canvas.h
+++
@@ -1,5 +0,0 @@
-```diff
-diff --git a/canvas.h b/canvas.h
-deleted file mode 100644
-index e69de29..0000000
-```
--- index.h
+++
@@ -1,5 +0,0 @@
-```diff
-diff --git a/index.h b/index.h
-deleted file mode 100644
-index e69de29..0000000
-```
